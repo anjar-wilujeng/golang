319eed90397ed6b83dd53e53c5103fd2d44c088a
anjar-wilujeng <118725585+anjar-wilujeng@users.noreply.github.com>
2023-05-22 17:30:27 +0700

    Update number.go
    Tipe data number

--- number.go
+++ number.go
@@ -3,22 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Satu =", 1)
-	fmt.Println("Dua =", 2)
-	fmt.Println("Tiga koma lima =", 3.5)
-
-	// boolean
-
-	fmt.Println("Benar =", true)
-	fmt.Println("Salah =", false)
-
-	// string
-	fmt.Println("Hello World")
-	fmt.Println("Tets String")
-	fmt.Println("Test")
-
-	fmt.Println(len("Test"))
-	fmt.Println("Saputra"[0])
-	fmt.Println("Saputra"[1])
-
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga koma lima = ", 3.5)
 }
