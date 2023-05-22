c2c59b11d28c4967128d01ee6bd0959905d0fa89
anjar-wilujeng <118725585+anjar-wilujeng@users.noreply.github.com>
2023-05-22 18:37:12 +0700

    Add files via upload

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Contoh String")
+	fmt.Println("Contoh String Dua")
+	fmt.Println("Contoh String Tiga")
+
+	fmt.Println(len("Contoh String"))
+	fmt.Println("Contoh String Dua"[0])
+	fmt.Println("Contoh String Tiga"[1])
+}
