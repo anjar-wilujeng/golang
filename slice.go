a7609c2b8a43a079cea785fff524931109daf48c
anjar wilujeng <anjarwilujeng8@gmail.com>
2023-05-27 16:55:55 +0700

    Update slice.go

--- slice.go
+++ slice.go
@@ -32,25 +32,28 @@ func main() {
 	var slice2 = months[11:]
 	fmt.Println(slice2)
 
+	//append slice
 	var slice3 = append(slice2, "Junaidi")
 	fmt.Println(slice3)
 	slice3[1] = "Bukan Desember"
 	fmt.Println(slice3)
 	fmt.Println(months)
-
+	
+	//make slice
 	newSlice := make([]string, 2, 5)
-
 	newSlice[0] = "Junaidi"
 	newSlice[1] = "Idris"
 
 	fmt.Println(newSlice)
 	fmt.Println(len(newSlice))
 	fmt.Println(cap(newSlice))
-
+	
+	//copy slice
 	copySlice := make([]string, len(newSlice), cap(newSlice))
 	copy(copySlice, newSlice)
 	fmt.Println(copySlice)
-
+	
+	//array vs slice
 	iniArray := [5]int{1, 2, 3, 4, 5}
 	iniSlice := []int{1, 2, 3, 4, 5}
 	fmt.Println(iniArray)
